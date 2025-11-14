c97c162d4eaf32f0cbf6a57fef12480d9a1615c3
your name <your email>
2025-11-14 01:19:09 -0700

    fix: clean up code

--- s3.tf
+++ s3.tf
@@ -19,22 +19,3 @@ resource "random_integer" "bucket_int" {
 }
 
 
-#event notification
-resource "aws_sqs_queue" "queue" {
-  name   = "s3-notification-queue"
-  policy = data.aws_iam_policy_document.queue.json
-}
-
-
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.lagos.id
-
-  queue {
-    queue_arn     = aws_sqs_queue.queue.arn
-    events        = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
-    filter_suffix = ".log"
-  }
-}
-
-
